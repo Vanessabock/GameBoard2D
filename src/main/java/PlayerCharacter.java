09094d5b9e04d70a6f3e5e403da1f4f188dc9cbc
Sviat <slsvyatko@gmail.com>
2023-11-30 12:09:10 +0100

    added test for moves "S", "D", "A" and changed function for "S", "D", "A" moves

--- src/main/java/PlayerCharacter.java
+++ src/main/java/PlayerCharacter.java
@@ -2,18 +2,22 @@ public class PlayerCharacter {
     static int x = 0;
     static int y = 0;
     public static int getX(){
-
         return x;
     }
 
     public static int getY() {
-
         return y;
     }
 
     public static void move(String s){
         if (s.equals("W")){
             y++;
+        } else if(s.equals("S")){
+            y--;
+        } else if(s.equals("D")){
+            x++;
+        } else if(s.equals("A")){
+            x--;
         }
     }
 }
--- src/test/java/PlayerCharacterTest.java
+++ src/test/java/PlayerCharacterTest.java
@@ -40,7 +40,47 @@ class PlayerCharacterTest {
         //THEN
         assertEquals(expectedY, actual);
     }
+    @Test
+    public void moveTest_whenS_thenGetYIs0(){
+        //GIVEN
+        int expectedY = 0;
+        String move = "S";
 
+        //WHEN
+        PlayerCharacter.move(move);
+        int actual = PlayerCharacter.getY();
+
+        //THEN
+        assertEquals(expectedY, actual);
+    }
+
+    @Test
+    public void moveTest_whenD_thenGetXIs1(){
+        //GIVEN
+        int expectedX = 1;
+        String move = "D";
+
+        //WHEN
+        PlayerCharacter.move(move);
+        int actual = PlayerCharacter.getX();
+
+        //THEN
+        assertEquals(expectedX, actual);
+    }
+
+    @Test
+    public void moveTest_whenA_thenGetXIs0(){
+        //GIVEN
+        int expectedX = 0;
+        String move = "A";
+
+        //WHEN
+        PlayerCharacter.move(move);
+        int actual = PlayerCharacter.getX();
+
+        //THEN
+        assertEquals(expectedX, actual);
+    }
 
 
 }
